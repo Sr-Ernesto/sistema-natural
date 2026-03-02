b97dbaddc21cc2f35f4f8a68bbf08c119688bcc3
Sr. Ernesto <srernestogomez@gmail.com>
2026-03-02 03:43:42 -0500

    feat: implement Modo Invisible for admin and fix tracking bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,11 +32,24 @@ export default function RootLayout({
       <head>
         <Script id="gtm-init" strategy="afterInteractive">
           {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            (function(w,d,s,l,i){
+              if (localStorage.getItem('osbrai_admin') === 'true') {
+                console.log('OpenClaw: Admin detected, GTM disabled.');
+                return;
+              }
+              w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','${GTM_ID}');
+          `}
+        </Script>
+        <Script id="admin-detector" strategy="beforeInteractive">
+          {`
+            if (window.location.search.includes('osbrai_admin=1')) {
+              localStorage.setItem('osbrai_admin', 'true');
+              alert('Modo Invisible Activado: Tu tráfico ya no será medido.');
+            }
           `}
         </Script>
       </head>
--- src/components/providers/TrackingProvider.tsx
+++ src/components/providers/TrackingProvider.tsx
@@ -10,6 +10,10 @@ export function TrackingProvider({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
 
   useEffect(() => {
+    if (localStorage.getItem('osbrai_admin') === 'true') {
+      console.log('OpenClaw: Admin detected, Facebook Tracking disabled.');
+      return;
+    }
     const eventId = Date.now().toString();
     
     // Server API PageView fallback
@@ -25,16 +29,18 @@ export function TrackingProvider({ children }: { children: React.ReactNode }) {
     <>
       <Script id="fb-pixel" strategy="afterInteractive">
         {`
-          !function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '${PIXEL_ID}');
-          fbq('track', 'PageView');
+          if (localStorage.getItem('osbrai_admin') !== 'true') {
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${PIXEL_ID}');
+            fbq('track', 'PageView');
+          }
         `}
       </Script>
       {children}
